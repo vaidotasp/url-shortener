99e3d690f26277f801c652f64cec18b3e29adb9a
Vaidotas <vp@Mac.local>
2017-08-13 08:13:34 -0400

    object constructor

--- app.js
+++ app.js
@@ -1,37 +1,74 @@
-const express = require('express');
-const mongodb = require('mongodb');
+const express = require('express')
+//const mongodb = require('mongodb')
 
-const app = express();
+const app = express()
 //REGEX for URL validation
-const re = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+const re = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
 
-let data = [];
+let data = [
+  {
+    id: 1234,
+    url: 'www.example.com',
+    output: 'www.url-short.com/1234'
+  },
+  {
+    id: 1999,
+    url: 'www.google.com',
+    output: 'www.url-short.com/1999'
+  }
+]
 
-function NewUrl(id, original, output) {
-  this.id = id;
-  this.original = original;
-  this.output = 'static' + this.id;
+// function NewUrl(url) {
+//   this.id = Math.floor(1000 + Math.random() * 9000)
+//   this.url = url
+//   this.output = 'static/' + this.id
+// }
+
+function NewUrl(url) {
+  this.id = Math.floor(1000 + Math.random() * 9000)
+  this.url = url
+  this.output = 'static/' + this.id
 }
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
+// let f = new NewUrl('www.miimimi.com')
+// console.log(f)
 
-app.get('/', function (req, res) {
-  res.send('works');
+app.get('/', function(req, res) {
+  res.send('works')
 })
 
-app.get('/new/:url*', function (req, res) {
-  let inputUrl = req.params['url'] + req.params['0'];
+//this should handle the redirect of the short URL id only
+app.get('/:id', function(req, res) {
+  //need to take the ID, check if it exists in DB, and redirect if it does
+  //if it does not, do nothing and end response
+  res.send(req.params.id)
+})
+
+//This should handle the new URL-Short requests only
+app.get('/new/:url*', function(req, res) {
+  let inputUrl = req.params['url'] + req.params['0']
+
   //perform validation of URL
   if (inputUrl.match(re) === null) {
-    throw new Error(inputUrl);
+    throw new Error(inputUrl)
   } else {
-    console.log(getRandomInt(0, 4))
-    res.send(inputUrl);
+    //check if url is already existing in DB
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].url === inputUrl) {
+        console.log('url already exists, here is the original output')
+        break
+      } else if (inputUrl !== data[i].url && i === data.length - 1) {
+        console.log('url does not exist, we will have to create one')
+        let o = new NewUrl(inputUrl)
+        console.log(o.id)
+        data[2] = o
+        console.log('object created')
+        break
+      }
+    }
+    //console.log(data)
+    console.log(data)
   }
 })
 
-app.listen(3000);
+app.listen(3000)
