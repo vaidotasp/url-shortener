0a5840f606f784f941c0596d28e9a0d85f412d6b
Vaidotas <vp@Mac.local>
2017-08-22 06:15:11 -0400

    MongoDB transition integration

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 const express = require('express')
 const app = express()
+const Short = require(__dirname + '/models/urlinstance')
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
 //REGEX for URL validation
 const re = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
-
-
-
-let data = [
-  {
-    id: 1234,
-    url: 'www.example.com',
-    output: 'http://www.google.com' //important to use http:// for the redirect to work
-  },
-  {
-    id: 1999,
-    url: 'www.google.com',
-    output: 'www.url-short.com/1999'
-  }
-]
 
 function NewUrl(url) {
   this.id = Math.floor(1000 + Math.random() * 9000)
@@ -25,43 +13,73 @@ function NewUrl(url) {
 }
 
 app.get('/', function(req, res) {
+  //creates a mock data collection
+  mongoose.connect('mongodb://shorturl:whyme@ds157712.mlab.com:57712/url', {
+    useMongoClient: true
+  })
+  let srt = new Short({
+    id: Math.floor(1000 + Math.random() * 9000),
+    url: 'www.wooopy.com',
+    output: 'www.static.com/' + '333ID'
+  })
+  srt.save(function(err) {
+    if (err) return err
+    console.log('Saved probably?')
+    //saved
+  })
+
   res.send('works')
 })
 
 //this should handle the redirect of the short URL id only
 app.get('/:id', function(req, res) {
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].id === Number(req.params.id)) {
-      res.redirect(data[i].output) //important to use http:// for the redirect to work
-    }
-  }
+  mongoose.connect('mongodb://shorturl:whyme@ds157712.mlab.com:57712/url', {
+    useMongoClient: true
+  })
+  let newId = Number(req.params.id)
+  Short.findOne({ id: newId }, function(err, result) {
+    if (err) return err
+    res.redirect(result.output)
+  })
 })
 
 //This should handle the new URL-Short requests only
 app.get('/new/:url*', function(req, res) {
+  mongoose.connect('mongodb://shorturl:whyme@ds157712.mlab.com:57712/url', {
+    useMongoClient: true
+  })
   let inputUrl = req.params['url'] + req.params['0']
+  function normalizeUrl(input) {
+    if (input[0] === 'w') {
+      return 'http://' + input
+    }
+    return input
+  }
 
-  //perform validation of URL
   if (inputUrl.match(re) === null) {
     throw new Error(inputUrl)
   } else {
-    //check if url is already existing in DB
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].url === inputUrl) {
-        console.log('url already exists, here is the original output')
-        res.send(data[i].output)
-        break
-      } else if (inputUrl !== data[i].url && i === data.length - 1) {
-        console.log('url does not exist, we will have to create one')
-        let o = new NewUrl(inputUrl)
-        console.log('object created')
-        data.push(o)
-        res.send(data[i].output)
-        break
+    let newUrl = normalizeUrl(inputUrl)
+    Short.findOne({ url: inputUrl }, function(err, result) {
+      if (err) console.log(err)
+      if (result === null) {
+        console.log('url is not found...')
+        //creating a new instance -- should probably check if id exists or not
+        let newEntry = new Short({
+          id: Math.floor(1000 + Math.random() * 9000),
+          url: newUrl,
+          output: 'http://www.static.com/' + this.id
+        })
+        //saving newly created instance
+        newEntry.save(function(err) {
+          if (err) console.log('Error: ', err)
+        })
+      } else {
+        //Handle if URL already exists in DB
+        console.log('url is found', result.url)
       }
-    }
-    console.log(JSON.stringify(data))
+    })
   }
 })
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || 3000)
