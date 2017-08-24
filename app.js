5b5e598f10a34680dc3a38c3d0809bbc7bc4ca1b
Vaidotas <vp@Mac.local>
2017-08-23 21:01:04 -0400

    changes to ouput JSON

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
+/*
+TODO:
+ - Figure out the .env issue so I can hide the path
+ - Use shortid pkg to genera unique short url ids
+ - Use mongoose-unique-validator to ipmrove the schema so that short urls are unique
+*/
 const express = require('express')
 const app = express()
 const Short = require(__dirname + '/models/urlinstance')
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
+app.use(express.static('public'))
 //REGEX for URL validation
 const re = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
 
 app.get('/', function(req, res) {
-  //creates a mock data collection
-  // mongoose.connect('mongodb://shorturl:whyme@ds157712.mlab.com:57712/url', {
-  //   useMongoClient: true
-  // })
-  // let srt = new Short({
-  //   id: Math.floor(1000 + Math.random() * 9000),
-  //   url: 'www.wooopy.com',
-  //   output: 'https://vp-url-short.herokuapp.com/' + '333ID'
-  // })
-  // srt.save(function(err) {
-  //   if (err) return err
-  //   console.log('Saved probably?')
-  //   //saved
-  // })
-
-  res.send('works')
+  res.sendFile(__dirname + '/index.html', function(err) {
+    if (err) throw err
+  })
 })
 
 //this should handle the redirect of the short URL id only
@@ -62,15 +56,28 @@ app.get('/new/:url*', function(req, res) {
         let newEntry = new Short({
           id: Math.floor(1000 + Math.random() * 9000),
           url: newUrl,
-          output: 'https://vp-url-short.herokuapp.com/' + this.id
+          output: 'https://vp-url-short.herokuapp.com/' + String(this.id)
         })
         //saving newly created instance
-        newEntry.save(function(err) {
-          if (err) console.log('Error: ', err)
-        })
+        newEntry
+          .save(function(err) {
+            if (err) console.log('Error: ', err)
+          })
+          .then(function() {
+            let output = {
+              original_url: newEntry.url,
+              short_url: newEntry.output
+            }
+            res.send(output)
+          })
       } else {
         //Handle if URL already exists in DB
         console.log('url is found', result.url)
+        let output = {
+          original_url: result.url,
+          short_url: result.output
+        }
+        res.send(output)
       }
     })
   }
